6ab012e0376fb92a7d6af91ebef30927ded5b584
No-Name-Bit <izangarciachico@gmail.com>
2025-02-20 20:52:34 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function redirectToGithub() {
+    window.location.href = "https://github.com/yourusername"; // Replace with your GitHub link
+}
+
+function redirectToLinkedIn() {
+    window.location.href = "https://linkedin.com/in/yourusername"; // Replace with your LinkedIn link
+}
