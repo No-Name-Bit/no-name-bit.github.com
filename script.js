bf643324fdf328b158ff6576e46dd53b99771ac6
Ethan <izangarciachico@gmail.com>
2025-02-21 13:08:26 +0100

    script.js

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function redirectToGithub() {
 function redirectToLinkedIn() {
     window.location.href = "https://www.linkedin.com/in/ethan-garcia-chico";
 }
+function redirectToTryHackMe() {
+    window.location.href = 'https://tryhackme.com/p/NoName0001';
+}
+function redirectToDiscord() {
+    window.location.href = 'https://discord.com/users/316675973389221890';
+}
